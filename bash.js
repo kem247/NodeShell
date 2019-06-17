93e03f1759bb2d23bba8facdd910aa6291400db4
Kemi Thomas <kemothom71@gmail.com>
2019-06-17 12:05:53 -0500

    fixed the ls

--- bash.js
+++ bash.js
@@ -1,6 +1,17 @@
 const pwdFile = require("./pwd");
 const lsFile = require("./ls");
+
 // const catFile = require("./cat");
-pwdFile();
-lsFile();
+// pwdFile();
+// lsFile();
 // catFile();
+
+process.stdout.write("prompt >");
+process.stdin.on("data", data => {
+  const cmd = data.toString().trim();
+  if (cmd === "pwd") {
+    pwdFile();
+  } else if (cmd === "ls") {
+    lsFile();
+  }
+});
--- pwd.js
+++ pwd.js
@@ -1,7 +1,6 @@
 module.exports = function() {
-  process.stdout.write("prompt >");
-  process.stdin.on("data", data => {
-    process.stdout.write(process.cwd());
-    process.stdout.write("\nprompt > ");
-  });
+  //   process.stdin.on("data", data => {
+  process.stdout.write(process.cwd());
+  process.stdout.write("\nprompt >");
+  //   });
 };
