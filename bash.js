9c2114ff92c4e3169cbde1d6b6a6a2259ad20050
Kemi Thomas <kemothom71@gmail.com>
2019-06-17 11:43:16 -0500

    all steps up until cat

---
+++ bash.js
@@ -0,0 +1,6 @@
+const pwdFile = require("./pwd");
+const lsFile = require("./ls");
+// const catFile = require("./cat");
+pwdFile();
+lsFile();
+// catFile();
---
+++ cat.js
@@ -0,0 +1,11 @@
+// module.exports = function(str, fileName) {
+//   const file = str.split(" ");
+
+//   console.log(file + "/");
+// };
+
+// process.stdout.write("prompt >");
+// process.stdin.on("data", data => {
+//   process.stdout.write(process.cwd());
+//   process.stdout.write("\nprompt > ");
+// });
